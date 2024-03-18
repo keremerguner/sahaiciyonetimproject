f2d473247d63a00607313c2cafc0357a0d7a95d5
Kerem Erguner <73108947+keremerguner@users.noreply.github.com>
2024-03-18 18:41:43 +0300

    firebase auth modülü eklendi

--- App.tsx
+++ App.tsx
@@ -1,118 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, {useState} from 'react';
+import {SafeAreaView, Text, Button} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const kayıtOl = () => {
+    auth()
+      .createUserWithEmailAndPassword('kerem@gm.com', 'kerem123')
+      .then(responsem => {
+        console.log('Kayıt olundu: ', responsem);
+      })
+      .catch(hata => {
+        console.log('cathe girdi', hata);
+        setFirebaseHata(hata.message);
+      });
+  };
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const girisYap = () => {
+    auth()
+      .signInWithEmailAndPassword('kerem@gm.com', 'kerem123')
+      .then(responsGiris => {
+        console.log('giris yaptı: ', responsGiris);
+      })
+      .catch(hataGiris => {
+        console.log('Hata: ', hataGiris);
+        setFirebaseHata(hataGiris.message);
+      });
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const cikisYap = () => {
+    auth()
+      .signOut()
+      .then(cikisThen => {
+        console.log('Cikis yaptı', cikisThen);
+        setAnlikKisi(''); // Çıkış yapınca anlık kişiyi temizle
+      })
+      .catch(cikisCatch => {
+        console.log('CikisCatch', cikisCatch);
+        setFirebaseHata(cikisCatch.message);
+      });
+  };
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+  const [anlikKisi, setAnlikKisi] = useState('');
+  const [firebaseHata, setFirebaseHata] = useState();
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const aktifKullanici = () => {
+    const anlikUser = auth().currentUser;
+    if (anlikUser) {
+      // Eğer anlikUser.email null ise boş bir string ata, değilse anlikUser.email değerini kullan
+      setAnlikKisi(anlikUser.email ? anlikUser.email : '');
+      console.log('Aktif kullanıcı:', anlikUser.email);
+    } else {
+      setAnlikKisi('Kullanıcı giriş yapmamış.');
+      console.log('Kullanıcı giriş yapmamış.');
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'azure',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text>Saha ici yonetim project</Text>
+      <Button title="Kayıt Ol" onPress={kayıtOl}></Button>
+      <Button title="Giriş Yap" onPress={girisYap}></Button>
+      <Button title="Çıkış yap" onPress={cikisYap}></Button>
+      <Button title="Aktif Kullanici" onPress={aktifKullanici}></Button>
+      {/* Anlık kullanıcıyı göster */}
+      <Text>-{anlikKisi}-</Text>
+      <Text>-{firebaseHata}-</Text>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
+};
 export default App;
+
+const res = {
+  additionalUserInfo: {
+    isNewUser: false,
+    profile: null,
+    providerId: 'password',
+    username: null,
+  },
+  user: {
+    displayName: null,
+    email: 'kerem@gm.com',
+    emailVerified: false,
+    isAnonymous: false,
+    metadata: [Object],
+    multiFactor: [Object],
+    phoneNumber: null,
+    photoURL: null,
+    providerData: [Array],
+    providerId: 'firebase',
+    refreshToken:
+      'AMf-vBy6WJDYxOVgGDiH1ZaTe3kbdHQzmmYp99drJIJq3SaL133SAi-IsdBt22oE44-Tr2hZsJeIIqnNqaXOX5CcgZlrxWtJWr7JyxX-9lRRSF5BaELa5uuvGIiLjtCQZs1-DxeyDz197nVVL4lK7QUA2rtMThtzKdtkB8_B87PmdsniD4N4QXe27mE8gfD-E3pbjDSWzg8h2Ju-0jhhLcbQtnilJIEB3w',
+    tenantId: null,
+    uid: 'cwD6n2PWH5fkSbxyQNsPtk8tg8a2',
+  },
+};
