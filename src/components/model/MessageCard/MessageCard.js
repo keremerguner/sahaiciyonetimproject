9435d0593fe40c8d73d962ea38ef5cca0cac95df
Kerem Erguner <73108947+keremerguner@users.noreply.github.com>
2024-04-22 11:14:02 +0300

    message card olusturuldu

--- src/components/model/MessageCard/MessageCard.js
+++ src/components/model/MessageCard/MessageCard.js
@@ -1,40 +1,24 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native'
-import styles from './MessageCard.style'
-import { formatDistance, parseISO } from 'date-fns';
-import { en } from 'date-fns/locale';
+import {View, Text, TouchableOpacity} from 'react-native';
+import styles from './MessageCard.style';
+import {formatDistance, parseISO} from 'date-fns';
+import {en, tr} from 'date-fns/locale';
 
-const MessageCard = ({ message, onPress }) => {
-    const formatedDate = formatDistance(parseISO(message.date), new Date(), {
-        addSuffix: true,
-        locale: en
-    })
+const MessageCard = ({message, onPress}) => {
+  const formatedDate = formatDistance(parseISO(message.date), new Date(), {
+    addSuffix: true,
+    locale: tr,
+  });
 
-    return (
-        <View style={styles.container}>
-            <View style={styles.inner_container}>
-                <Text style={styles.user}>Orderer: {message.orderMaker}</Text>
-                <Text style={styles.date}>Create Time: {formatedDate}</Text>
-            </View>
-
-            <Text style={styles.title}>Product Name: {message.productName}</Text>
-            <Text style={styles.title}>Product Color: {message.productColor}</Text>
-            <Text style={styles.title}>Product Piece: {message.productPiece}</Text>
-            <Text style={styles.title}>Product Dimension: {message.productDimension}</Text>
-            <Text style={styles.title}>Worker Name: {message.workerName}</Text>
-            <Text style={styles.title}>Status: {message.status}</Text>
-            <View style={styles.footer}>
-                <TouchableOpacity style={styles.dislike_container} onPress={onPress} >
-                    {!!message.dislike && (
-                        <View style={styles.dislike_count_container}>
-                            <Text style={styles.dislike_count_text}>{message.dislike}</Text>
-                        </View>
-                    )}
-                    <Text style={styles.dislike_text}>DONE</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <View style={styles.inner_container}>
+        <Text style={styles.user}>Username: {message.username}</Text>
+        <Text style={styles.date}>{formatedDate}</Text>
+      </View>
+      <Text>Mesaj: {message.text} </Text>
+    </View>
+  );
+};
 
 export default MessageCard;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,26 @@
-import React, {useState} from 'react';
-import {Text, View, SafeAreaView, TouchableOpacity} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, View, SafeAreaView, TouchableOpacity, FlatList} from 'react-native';
 import FloatingButton from '../../components/FloatingButton';
 import ContentInputModal from '../../components/model/ContentInput/ContentInputModal';
 import auth from '@react-native-firebase/auth';
+import database from '@react-native-firebase/database';
+import parserContentData from '../../utils/parserContentData';
+import MessageCard from '../../components/model/MessageCard';
 
 const Home = () => {
   const [inputModalVisible, setInputModalVisible] = useState(false);
+  const [contentList, setContentList] = useState([]);
+
+
+  useEffect(() => {
+    database().ref('messages/').on('value', snapshot => {
+      const contentData = snapshot.val();
+      console.log('content data: ', contentData);
+      const parsedData = parserContentData(contentData || {})
+
+      setContentList(parsedData)
+    });
+  },[])
 
   function handleInputToggle() {
     setInputModalVisible(!inputModalVisible);
@@ -14,7 +29,26 @@ const Home = () => {
   function handleSendContent(content) {
     handleInputToggle();
     console.log(content);
+    sendContent(content)
   }
+
+  function sendContent(content){
+    const userMail = auth().currentUser.email;
+
+    const contentObject = {
+      text: content,
+      username: userMail.split('@')[0],
+      date: new Date().toISOString(),
+    };
+
+    console.log("gonderilen obje: ",contentObject)
+    
+    database().ref('messages/').push(contentObject)
+  }
+
+
+  const renderContent = ({item}) => <MessageCard message={item} />
+
 
   return (
     <SafeAreaView
@@ -36,6 +70,10 @@ const Home = () => {
         }}>
         Home
       </Text>
+      <FlatList
+      data={contentList}
+      renderItem={renderContent}
+       />
       <TouchableOpacity
         onPress={() => {
           auth().signOut();
